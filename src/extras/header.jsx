1a9f86dd721fe30673564da78398bdce05541271
DanielTheDoc <danielgaldan9@gmail.com>
2025-05-14 15:52:49 +0800

    Initial commit

---
+++ src/extras/header.jsx
@@ -0,0 +1,12 @@
+import './header.css'
+
+function Header(){
+    return(
+        <>
+            <div className='header'>
+                
+            </div>
+        </>
+    )
+}
+export default Header;
