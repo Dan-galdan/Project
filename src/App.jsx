639006452d4ecebd4e6289a7bfcccce9e36a2cef
Dan-galdan <danielgaldan9@gmail.com>
2025-06-02 17:52:42 +0800

    let me try someting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home.jsx'
+// App.jsx
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { AnimatePresence } from 'framer-motion';
+
+import Home from './pages/home.jsx';
 import Popular from './pages/Popular.jsx';
-import Top from './pages/top rated.jsx'
+import Top from './pages/top rated.jsx';
+
+function AnimatedRoutes() {
+  const location = useLocation();
+
+  return (
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Home />} />
+        <Route path="/popular" element={<Popular />} />
+        <Route path="/top_rated" element={<Top />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/popular' element={<Popular/>}></Route>
-        <Route path='/top_rated' element={<Top/>}></Route>
-      </Routes>
+      <AnimatedRoutes />
     </Router>
   );
 }
