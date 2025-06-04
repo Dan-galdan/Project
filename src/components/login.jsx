9c7b455a08ec3aa82a9fe3520a7f973f8baa6c11
DanielTheDoc <danielgaldan9@gmail.com>
2025-06-05 07:59:51 +0800

    balls

---
+++ src/components/login.jsx
@@ -0,0 +1,14 @@
+import './header.css';
+import '../index.css';
+import { useState, useEffect, useRef } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+
+function Login(){
+    return(
+        <>
+            <div className="loginBlur">
+                
+            </div>
+        </>
+    )
+}
